0fee17bc52d53b2d0bdd2dbb054a2bf4768591af
shivamkathait19 <kathaitshivam151@gmail.com>
2025-01-02 22:37:06 +0530

    Add a code

--- lib/view/widget/Newscontainer.dart
+++ lib/view/widget/Newscontainer.dart
@@ -1,25 +1,59 @@
 import 'package:flutter/material.dart';
+ class Newscontainer extends StatelessWidget {
+   String imgUrl;
+   String newsHead;
+   String newswdes;
+   String newURl;
+    Newscontainer({super.key,
+    required this.imgUrl,
+    required this.newsHead,
+    required this.newswdes,
+    required this.newURl,
+    });
 
-class Newscontainer extends StatelessWidget {
-  String imgUrl;
-  String newhead ;
-  String newDes;
-  const Newscontainer({super.key});
+   @override
+   Widget build(BuildContext context) {
+     return Container(
+       height: MediaQuery.of(context).size.height,
+       width: MediaQuery.of(context).size.width,
+       child:
+          Column(
+             crossAxisAlignment: CrossAxisAlignment.start,
+             children: [
+           Image.network( height: 400, width: MediaQuery.of(context).size.width,fit: BoxFit.cover ,imgUrl),
+           Container(
+             padding: EdgeInsets.symmetric(horizontal: 20),
+             child: Column(
+               crossAxisAlignment:CrossAxisAlignment.start,
+               children: [
+             SizedBox(height: 20,),
+             Text("Vodafone Idea to launch 5G services by March 2025,",),
+             Text(newsHead,style: TextStyle(fontSize: 20 ,fontWeight: FontWeight.bold),),
+             SizedBox(height: 10,),
+             Text(newswdes,style: TextStyle(fontSize: 10,),),
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
+           ],),),
 
+             Spacer(),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.end,
+             children: [
+               Column(
 
-      child: Column(children: [
-        Image.network("src"),
-        Text("New heading"),
-        Text("News Desecription"),
-        ElevatedButton(onPressed:(){},
-            child: Text("Read more"))
-      ],),
-    );
-  }
-}
+                 children: [
+                   Padding(
+                     padding: const EdgeInsets.symmetric(horizontal: 25),
+                     child: ElevatedButton(onPressed: (){
+                       print("GOING TO $newURl");
+                     }, child: Text("Read more")),
+                   ),
+                 ],
+               ),
+             ],
+           ),
+               SizedBox(height: 20,),
+         
+         ]),
+     );
+    }
+ }
