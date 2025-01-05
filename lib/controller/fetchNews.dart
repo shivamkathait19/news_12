f754d721a37233c48805c1c638fd21a10f450d62
shivamkathait19 <kathaitshivam151@gmail.com>
2025-01-05 21:25:28 +0530

    update a new code

--- lib/controller/fetchNews.dart
+++ lib/controller/fetchNews.dart
@@ -1,9 +1,56 @@
-// https://newsapi.org/v2/top-headlines/sources?apiKey=f85d16b212f7435a8af5c17f6119453e
+ //https://newsapi.org/v2/top-headlines/sources?apiKey=f85d16b212f7435a8af5c17f6119453e
 import 'dart:convert';
+import 'dart:math';
 import 'package:http/http.dart';
 class FetchNews {
+  static List sources = [
+    "abc-news",
+
+    "bbc-news",
+    "bbc-sport",
+
+    "business-insider",
+
+    "engadget",
+    "entertainment-weekly",
+    "espn",
+    "espn-cric-info",
+    "financial-post",
+
+    "fox-news",
+    "fox-sports",
+    "globo",
+    "google-news",
+    "google-news-in",
+
+    "medical-news-today",
+
+    "national-geographic",
+
+    "news24",
+    "new-scientist",
+
+    "new-york-magazine",
+    "next-big-future",
+
+    "techcrunch",
+    "techradar",
+
+    "the-hindu",
+
+    "the-wall-street-journal",
+
+    "the-washington-times",
+    "time",
+    "usa-today",
+  ];
   static fetchNews() async{
-   Response response = await get(Uri.parse("// https://newsapi.org/v2/top-headlines/sources?apiKey=f85d16b212f7435a8af5c17f6119453e"
+    final _random = new Random();
+    var element = sources[_random.nextInt(sources.length)];
+
+print(element);
+
+   Response response = await get(Uri.parse(" https://newsapi.org/v2/top-headlines/sources?apiKey=f85d16b212f7435a8af5c17f6119453e"
        ));
 Map body_data = jsonDecode(response.body);
 print(body_data);
