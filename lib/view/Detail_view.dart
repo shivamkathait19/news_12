172a21c63f1abe1de934004a4139a74f378fc53a
shivamkathait19 <kathaitshivam151@gmail.com>
2025-01-08 19:35:36 +0530

    Add

--- lib/view/Detail_view.dart
+++ lib/view/Detail_view.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class DetailViewScreen extends StatelessWidget {
+class DetailViewScreen extends StatefulWidget {
   String newUri;
   DetailViewScreen({super.key,required this.newUri});
 
   @override
+  State<DetailViewScreen> createState() => _DetailViewScreenState();
+}
+
+class _DetailViewScreenState extends State<DetailViewScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center( child:Text(newUri),),
+      body: WebView(
+        initialUrl:widget.newUri,
+  javascriptMode : JavaScriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController){
+        }
+      )
     );
-    
+
   }
 }
 
