0194533cdfe652ff3bef5fd300aad1bbd5080304
shivamkathait19 <kathaitshivam151@gmail.com>
2025-01-10 20:10:24 +0530

    Add

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:news_12/view/home.dart';
+import 'package:news_12/view/splashScreen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  LoadHome(){
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+    });
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    LoadHome();
+  }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -18,7 +36,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: splashScreen(),
     );
   }
 }
