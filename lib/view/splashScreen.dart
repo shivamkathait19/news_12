9d6e67bc6376913d83fb3a8347d4b1ae5f93cf55
shivamkathait19 <kathaitshivam151@gmail.com>
2025-01-11 22:01:44 +0530

    Add

--- lib/view/splashScreen.dart
+++ lib/view/splashScreen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
  class splashScreen extends StatelessWidget {
    const splashScreen({super.key});
- 
+
    @override
    Widget build(BuildContext context) {
      return Scaffold(
-       body: Center(child: Text("News Snak"),),
+       body: Center(child: Text(" NEWS SNAKS",style: TextStyle(fontSize: 17),
+       ),
+
+
+       ),
+
      );
    }
- }
+}
+
+
  
