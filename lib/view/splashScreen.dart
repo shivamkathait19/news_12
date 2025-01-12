b3231a66a0693695b0b465bde46f122b94b63ba3
shivamkathait19 <kathaitshivam151@gmail.com>
2025-01-12 20:13:20 +0530

    Add a code

--- lib/view/splashScreen.dart
+++ lib/view/splashScreen.dart
@@ -5,12 +5,29 @@ import 'package:flutter/material.dart';
    @override
    Widget build(BuildContext context) {
      return Scaffold(
-       body: Center
-         (child:
-       Text(" NEWS Snaks ",style: TextStyle(fontSize: 17),
-       ),
-     ),
-     );
+       backgroundColor: Colors.grey,
+              body: Center(
+               child: Column(
+                 children:[
+                   SizedBox(
+                     height: 200,
+                   ),
+                   Image.network ("https://static.toiimg.com/photo/msid-104408881/104408881.jpg?1326042",),
+                 Text("News Snaks" ,style: TextStyle(fontSize: 20),),
+                 Spacer(),
+                 Text("APP CREATE BY ",style: TextStyle(fontSize: 17),),
+                   Text("shubham ",style: TextStyle(fontSize: 15),),
+                   SizedBox(
+                     height: 15,
+                   )
+                      ],
+                      ),
+
+               ),
+               );
+
+
+
    }
 }
 
